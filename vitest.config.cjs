549cd27f8d564621fe17ff551a46ada3406d6f98
CircleCI Agent <circleci-agent[bot]@users.noreply.github.com>
2025-07-25 21:28:05 +0000

    Fix flaky tests

---
+++ vitest.config.cjs
@@ -0,0 +1,16 @@
+const { defineConfig } = require('vitest/config')
+
+module.exports = defineConfig({
+  test: {
+    globals: true,
+    environment: 'node',
+    include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    reporters: ['default', 'junit'],
+    outputFile: {
+      junit: './test-results/junit.xml'
+    },
+    shard: process.env.VITEST_SHARD, // Format: `{index}/{total}`
+    maxThreads: 1, // Ensure consistent timing data
+    minThreads: 1
+  },
+})
